63f9afc5bad09ccc9670cfb08f3d2167441a9102
mistlog <mistlog@outlook.com>
2020-03-07 19:34:14 +0800

    add sveltestrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Link from "./component/Link";
+import { Button } from "sveltestrap";
 
-export interface IAppProps
-{
+export interface IAppProps {
     name: string;
 }
 
-export default function App(props: IAppProps)
-{
+export default function App(props: IAppProps) {
     const { name } = props;
 
     <main>
-        <h1>Hello {name}!</h1>
-        <p>Visit the <Link url="https://svelte.dev/tutorial" text="Svelte tutorial"/> to learn how to build Svelte apps.</p>
+        <Button color="primary" outline>Hello World!</Button>
+        <h1> Hello {name}!</h1>
+        <p>Visit the <Link url="https://svelte.dev/tutorial" text="Svelte tutorial" /> to learn how to build Svelte apps.</p>
     </main>
 }
